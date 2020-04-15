fc901b67fa445ead64aa8e603470ba343caa8091
Jane Cho <dragoncave48@gmail.com>
2020-04-15 16:14:03 -0500

    Jane Main Menu
    Add main menu, pause/resume, inventory

---
+++ Colin_movement/Team 5 Game MainMenu/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject ItemButton;
+    public Vector2 v;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.Slots.Length; i++)
+            {
+                if (inventory.IsFull[i] == false)
+                {
+                    inventory.IsFull[i] = true;
+                    //GameObject ItemCopy = Instantiate(ItemButton, inventory.Slots[i].transform, false);//copy object, make child, worldspace
+                    //ItemCopy.transform.position = inventory.Slots[i].transform.position;//set position
+                    Instantiate(ItemButton, inventory.Slots[i].transform);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
