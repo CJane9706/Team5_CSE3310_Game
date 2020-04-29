a935b4b62c9bbd8c4038282b6b459539078b6937
Jane Cho <dragoncave48@gmail.com>
2020-04-28 22:41:50 -0500

    Pause
    Pause/Resume, Options, Back to main menu

---
+++ Team5_CSE3310_Game_0428/Team 5 Game/Assets/RavenQuest/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main Scene",LoadSceneMode.Single);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
